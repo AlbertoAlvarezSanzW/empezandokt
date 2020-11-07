dc2c29a0a9fb39bd45d78f0cde282d09cb6faeb5
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-07 21:07:35 +0100

    update

--- src/Main.kt
+++ src/Main.kt
@@ -334,10 +334,38 @@ fun main(){
 
     println("\n\nFunciones: let, run, with")
     /*
+    Imaginado que existe una variable i.
 
-     */
+    with(i){...}
+
+    i.let {}
+
+    run {} o i.run{}
+
+    Estas 3 funciones devuelven la última línea de código de la Lambda.
+    Son muy útiles en conjunto a ?
+    i?.let { println("Este mensaje no aparecerá ya que i es null") }
+
+    */
+    i.let{}
+    run{}
+    i.run{}
+
+    println("\n\nFunciones apply, also")
+    // estas 2 funciones devolverian i
+    i.also {}
+    i.also {}
+    println("\n\n")
+    functionRun()
+    functionWith()
+    functionLet()
+    functionAlso()
+    functionApply()
+    functionLetVsAlso()
 
 
+    //Interfaz
+    println("\n\n")
 
 
     println("\n\n\n\n")
@@ -348,11 +376,6 @@ fun main(){
     }
 
 
-
-
-
-
-
 // función lambda
 fun example(functionAsParameter: (Int, Int) -> Int) : Int {
     return functionAsParameter(2,1)
@@ -361,6 +384,128 @@ fun example(functionAsParameter: (Int, Int) -> Int) : Int {
 // Mas funciones lamdba
 fun ejercicio1() {
     println("Hola")
+}
+
+class ClaseEjemplo{
+    var int1 : Int = 0
+    var int2 : Int = 0
+}
+
+fun functionRun(){
+    println("--- functionRun ---")
+
+    val i = ClaseEjemplo()
+
+    run {
+        i.int1++
+        i.int2++
+        println("El valor de i dentro del run1 es de ${i.int1} y ${i.int2}")
+    }
+    println("El valor de i fuera del run1 es de ${i.int1} y ${i.int2}")
+    i.run {
+        int1++
+        int2++
+        println("El valor de i dentro del run2 es de $int1 y $int2")
+    }
+    println("El valor de i fuera del run2 es de ${i.int1} y ${i.int2}")
+
+}
+
+fun functionWith(){
+    println("--- functionWith ---")
+
+    val i = ClaseEjemplo()
+
+    with(i){
+        int1++
+        int2++
+        println("El valor de i dentro del with es de $int1 y $int2")
+    }
+    println("El valor de i fuera del with es de ${i.int1} y ${i.int2}")
+}
+
+fun functionLet(){
+    println("--- functionLet ---")
+    var i : ClaseEjemplo? = null
+    i?.let { println("Este mensaje no aparecerá ya que i es null") }
+
+    i  = ClaseEjemplo()
+    i.let {
+        it.int1++
+        it.int2++
+        println("El valor de i dentro del let1 es de ${it.int1} y ${it.int2}") }
+    println("El valor de i fuera del let1 es de ${i.int1} y ${i.int2}")
+
+    i.let {
+        i  = ClaseEjemplo()
+        it.int1++
+        it.int2++
+        println("El valor de i dentro del let2 es de ${it.int1} y ${it.int2}")
+        "Este es el resultado de let"
+    }
+    println("El valor de i fuera del let es de ${i?.int1} y ${i?.int2}")
+    val resultado = i.let {
+        it?.int1
+    }
+    println("El resultado de let es $resultado")
+
+    i?.let {contador ->
+        println("El numero recibido es $contador")
+        // No se puede cambiar el valor de it.
+    }
+}
+
+fun functionAlso(){
+    println("--- functionAlso ---")
+    var i : ClaseEjemplo? = null
+    i?.also { println("Este mensaje no aparecerá ya que i es null") }
+
+    i  = ClaseEjemplo()
+    i.also {
+        it.int1++
+        it.int2++
+        println("El valor de i dentro del let1 es de ${it.int1} y ${it.int2}") }
+    println("El valor de i fuera del let1 es de ${i.int1} y ${i.int2}")
+
+    i.also {
+        i  = ClaseEjemplo()
+        it.int1++
+        it.int2++
+        println("El valor de i dentro del also2 es de ${it.int1} y ${it.int2}")
+    }
+    println("El valor de i fuera del also2 es de ${i?.int1} y ${i?.int2}")
+    val resultado = i.also {
+        it?.int1
+    }
+    println("El resultado de also3 es ${resultado?.int1}")
+    i.also { i?.int1 = 9 }.also { if (i?.int1 == 9) i?.int1 = 8 }
+    println("El resultado de also4 es ${resultado?.int1}")
+
+}
+
+fun functionLetVsAlso(){
+    println("--- functionLetVsAlso ---")
+
+    val i = ClaseEjemplo()
+    val resultado1 = i.let {
+        it.int1
+    }
+    println("El resultado de let es ${resultado1::class.java}")
+    val resultado2 = i.also {
+        it.int1
+    }
+    println("El resultado de also es ${resultado2::class.java}")
+}
+
+fun functionApply() {
+    println("--- functionApply ---")
+    val i = ClaseEjemplo()
+    i.apply {
+        int1++
+        int2++
+        println("El valor de i dentro del apply es de $int1 y $int2")
+    }
+    println("El valor de i fuera del apply es de ${i.int1} y ${i.int2}")
 }
 
 fun ejercicio2(integer : Int) {
